967c3970e1fcfed2824a96a379e6f9a81e32e2a6
timothycarambat <rambat1010@gmail.com>
2016-09-25 17:12:27 -0500

    Inital commit of working documents

---
+++ js/CustomIndex.js
@@ -0,0 +1,18 @@
+// Custom jquery for website
+ $("#Forensicbtn").hover(function() {
+      $(this).css('color', "#ff9900");
+    },function() {
+      $(this).css('color', "#fff");
+  });
+  
+   $("#Enginebtn").hover(function() {
+      $(this).css('color', "#4183D7");
+    },function() {
+      $(this).css('color', "#fff");
+  });
+  
+  $("#Businessbtn").hover(function() {
+      $(this).css('color', "#339966");
+    },function() {
+      $(this).css('color', "#fff");
+  });
